9b9f779857a3dfb83973a432670d0efc75604a64
86kkd <2973720272@qq.com>
2024-01-11 20:32:13 +0800

    init

---
+++ dist/loop.js
@@ -0,0 +1,23 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+
+    if (Object.keys(Game.creeps) == 0) {
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], 'Harvester1', { memory: { role: 'harvester' } });
+
+    }
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+};
