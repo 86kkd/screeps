41f5ef4cbcd8f976f6855d70e2a370a2a22eac96
86kkd <2973720272@qq.com>
2024-01-12 19:21:34 +0800

    自动产兵

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,44 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+const creep_type = ["harvester","upgrader","builder"];
 
+var spawn=Game.spawns['Spawn1'];
+// TODO开采地区选择
 
-
+function count_num_creeps(){
+    var num_creeps = {
+        harvester: 0,
+        upgrader: 0,
+        builder: 0
+      };
+    for(var name in Game.creeps){
+        if(name.includes('Harvester'))num_creeps.harvester+=1;
+        else if(name.includes('Upgrader'))num_creeps.upgrader+=1;
+        else if(name.includes('Builder'))num_creeps.builder+=1;
+    }
+    return num_creeps;
+}
 module.exports.loop = function () {
 
-    if(Object.keys(Game.creeps).length<=3){
-        
-        Game.spawns['Spawn1'].spawnCreep( [WORK,CARRY,MOVE],     'Harvester'+Object.keys(Game.creeps).length+1,     { memory: { role: 'harvester' } } );
+    var num_creeps = count_num_creeps();
+    
+    if(num_creeps.harvester<3){
+        spawn.spawnCreep( [WORK,WORK,CARRY,MOVE],'Harvester'+(num_creeps.harvester+1), { memory: { role: 'harvester' } } );
+    }
+    else if(num_creeps.upgrader<3){
+        spawn.spawnCreep( [WORK,WORK,CARRY,MOVE],'Upgrader'+(num_creeps.upgrader+1),{memory:{role:'upgrader'}});
+    }
+    else if(num_creeps.builder<5||spawn.store.getFreeCapacity()){
+        spawn.spawnCreep( [WORK,WORK,CARRY,MOVE],'Builder'+(num_creeps.builder+1),{memory:{role:'builder'}});
+        // console.log('spawnCreep signal '+success);
     }
     for(var name in Game.creeps) {
-        var creep = Game.creeps[name];if(Object.keys(Game.creeps)==0){
-    Game.spawns['Spawn1'].spawnCreep( [WORK,CARRY,CARRY,MOVE,MOVE],     'Harvester1',     { memory: { role: 'harvester' } } );
-}
+        
+        var creep = Game.creeps[name];
+        if (!creep_type.includes(creep.memory.role)){
+            console.log(creep.memory.role + " is not in creep_type")
+        }
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -2,16 +2,22 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.store.getFreeCapacity() > 0) {
+        var spawn=Game.spawns['Spawn1'];
+        if(creep.store[RESOURCE_ENERGY]==0&&creep.memory.building){
+            creep.memory.building=false;
+        }
+
+	    if(creep.store.getFreeCapacity() > 0&&!creep.memory.building) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
-        else {
+        else if(spawn.store.getFreeCapacity(RESOURCE_ENERGY)){
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                        return (structure.structureType == STRUCTURE_EXTENSION || 
+                            structure.structureType == STRUCTURE_SPAWN) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
@@ -21,6 +27,16 @@ var roleHarvester = {
                 }
             }
         }
+        else{
+            creep.memory.building = true;
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+        
 	}
 };
 
