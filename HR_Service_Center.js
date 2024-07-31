dcc48b95ad745e7449a6c541ad16b0e67d202e1e
86kkd <2973720373@qq.com>
2024-07-31 21:03:44 +0800

    init

--- HR_Service_Center.js
+++ HR_Service_Center.js
@@ -1,33 +1,31 @@
-const roleHarvester = require('role.harvester');
-const roleUpgrader = require('role.upgrader');
-const roleBuilder = require('role.builder');
-const roleTower = require('role.tower');
+const roleHarvester = require("role.harvester");
+const roleUpgrader = require("role.upgrader");
+const roleBuilder = require("role.builder");
+const roleTower = require("role.tower");
 
 const tover_id = "6cf4753c8d85837";
 
 let room_targets_ctl = {
+    source_target: null,
 
-    source_target:null,
-    
     /** @param {Creep} creep **/
-    search: (creep) =>{
-      // sys_log('bug here');
-      this.source_target = creep.room.find(FIND_SOURCES);
-      // sys_log('bug not here');
-      return this;   
-    }
-  };
+    search: (creep) => {
+        // sys_log('bug here');
+        this.source_target = creep.room.find(FIND_SOURCES);
+        // sys_log('bug not here');
+        return this;
+    },
+};
 
-let source_to_harvest={
-    builder1_source:1
+let source_to_harvest = {
+    builder1_source: 1,
 };
 
 class HRSC {
-
     /**
      * @function constructer initialize the creep_type
      */
-    constructor(){
+    constructor() {
         // this.creep_type = ["harvester","upgrader","builder"];
     }
 
@@ -35,31 +33,33 @@ class HRSC {
      * @function run the main loop
      */
     run() {
-        const creep_type = ["harvester","upgrader","builder"];
-        
-        let room_source =Game.spawns["Spawn1"].room.find(FIND_SOURCES);
+        const creep_type = ["harvester", "upgrader", "builder"];
+
+        const room_source = Game.spawns["Spawn1"].room.find(FIND_SOURCES);
 
         roleTower.run(tover_id);
-        for(var name in Game.creeps) {
-        
+        for (var name in Game.creeps) {
             var creep = Game.creeps[name];
-            if (!creep_type.includes(creep.memory.role)){
-                sys_log(creep.memory.role + " is not in creep_type")
+            if (!creep_type.includes(creep.memory.role)) {
+                sys_log(creep.memory.role + " is not in creep_type");
             }
-            if(creep.memory.role == 'harvester') {
+            if (creep.memory.role == "harvester") {
                 var targets = room_targets_ctl.search(creep);
-                roleHarvester.run(creep,room_source[0]);
+                roleHarvester.run(creep, room_source[0]);
             }
-            if(creep.memory.role == 'upgrader') {
-                roleUpgrader.run(creep,room_source[1]);
+            if (creep.memory.role == "upgrader") {
+                roleUpgrader.run(creep, room_source[1]);
             }
-            if(creep.memory.role == 'builder') {
-                roleBuilder.run(creep,room_source[1]);
+            if (creep.memory.role == "builder") {
+                roleBuilder.run(creep, room_source[1]);
             }
         }
     }
-};
+}
 
-module.exports = {HRSC,
-    CreateHRSC(){return new HRSC();}
+module.exports = {
+    HRSC,
+    CreateHRSC() {
+        return new HRSC();
+    },
 };
--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const { CreateHRSC } = require('./HR_Service_Center');
 
 var spawn=Game.spawns['Spawn1'];
 // TODO开采地区选择
-var source_to_harvest={
-    builder1_source:1
+var source_to_harvest = {
+    builder1_source: 1,
 };
 // tower_ids
 var tower_ids = {
-tower1:"6cf4753c8d85837"
+    tower1: "6cf4753c8d85837",
 };
-require('HR_Service_Center');
+require("HR_Service_Center");
 
 //TODO rewrite creep_counter and finish group
 //TODO source target ctl center
@@ -25,111 +25,233 @@ require('HR_Service_Center');
 //TODO tombstone recycle
 //注释
 
-// Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER ); 
+// Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
 var stage_ploy = {
-    test : "test",
-    ploy:[
-        {   
-            worker_body:[
-                WORK,WORK,
+    test: "test",
+    ploy: [
+        {
+            worker_body: [
+                WORK,
+                WORK,
                 CARRY,
-                MOVE],
-            num_harvester:6,
-            num_upgrader:6,
-            num_builder:4,
-            level:1
+                MOVE,
+            ],
+            num_harvester: 6,
+            num_upgrader: 6,
+            num_builder: 4,
+            level: 1,
         },
         {
-            worker_body:[
-                WORK,WORK,WORK,
-                CARRY,CARRY,
-                MOVE,MOVE,MOVE],
-            num_harvester:4,
-            num_upgrader:5,
-            num_builder:1,
-            level:2
+            worker_body: [
+                WORK,
+                WORK,
+                WORK,
+                CARRY,
+                CARRY,
+                MOVE,
+                MOVE,
+                MOVE,
+            ],
+            num_harvester: 4,
+            num_upgrader: 5,
+            num_builder: 1,
+            level: 2,
         },
         {
-            worker_body:[
-                WORK,WORK,WORK,WORK,
-                CARRY,CARRY,CARRY,CARRY,
-                MOVE,MOVE,MOVE,MOVE],
-            num_harvester:4,
-            num_upgrader:6,
-            num_builder:1,
-            level:3
+            worker_body: [
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+            ],
+            num_harvester: 4,
+            num_upgrader: 6,
+            num_builder: 1,
+            level: 3,
         },
         {
-            worker_body:[
-                WORK,WORK,WORK,WORK,WORK,WORK,WORK,
-                CARRY,CARRY,CARRY,CARRY,CARRY,
-                MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE],
-            num_harvester:2,
-            num_upgrader:3,
-            num_builder:1,
-            level:4
+            worker_body: [
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+            ],
+            num_harvester: 2,
+            num_upgrader: 3,
+            num_builder: 1,
+            level: 4,
         },
         {
-            worker_body:[
-                WORK,WORK,WORK,WORK,WORK,WORK,WORK,
-                CARRY,CARRY,CARRY,CARRY,CARRY,
-                MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE],
-            num_harvester:4,
-            num_upgrader:4,
-            num_builder:2,
-            level:5
-        
+            worker_body: [
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+            ],
+            num_harvester: 4,
+            num_upgrader: 4,
+            num_builder: 2,
+            level: 5,
         },
         {
-            worker_body:[
-                WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,
-                CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,
-                MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE],
-            num_harvester:4,
-            num_upgrader:4,
-            num_builder:2,
-            level:6
-         
-        }, 
-        {
-            worker_body:[
-                WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,
-                CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,
-                MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE],
-            num_harvester:4,
-            num_upgrader:4,
-            num_builder:2,
-            level:7
-
+            worker_body: [
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+            ],
+            num_harvester: 4,
+            num_upgrader: 4,
+            num_builder: 2,
+            level: 6,
         },
         {
-            worker_body:[
-                WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,
-                CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,
-                MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE],
-            num_harvester:4,
-            num_upgrader:4,
-            num_builder:2,
-            level:8     
+            worker_body: [
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+            ],
+            num_harvester: 4,
+            num_upgrader: 4,
+            num_builder: 2,
+            level: 7,
         },
-     ],
-    choise:function(room_level)  {
-       return this.ploy[room_level-1]
-    }
-}
-
+        {
+            worker_body: [
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                WORK,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                CARRY,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+                MOVE,
+            ],
+            num_harvester: 4,
+            num_upgrader: 4,
+            num_builder: 2,
+            level: 8,
+        },
+    ],
+    choise: function (room_level) {
+        return this.ploy[room_level - 1];
+    },
+};
 
 module.exports.loop = function () {
     var counter = new creep_counter.count();
     // sys_log(`counter success ${counter.count()}`);j
-//    sys_log(counter.harvester+'counter.harvester');
+    //    sys_log(counter.harvester+'counter.harvester');
     var room_level = spawn.room.controller.level;
-    sys_log("roomlevel"+room_level);
-    init_serval_workers(spawn,counter,
-        stage_ploy.choise(1));
-        
+    sys_log("roomlevel" + room_level);
+    init_serval_workers(spawn, counter, stage_ploy.choise(1));
+
     roleTower.run(tower_ids.tower1);
-    let HR_Service_Center = CreateHRSC();
+    const HR_Service_Center = CreateHRSC();
     sys_log(`HR_Service_Center ${HR_Service_Center.creep_type}`);
     HR_Service_Center.run();
-}
+};
