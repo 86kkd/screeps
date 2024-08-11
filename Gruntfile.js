edd8b593f0889529840bd6c0ff6f52188b05b7c7
86kkd <2973720373@qq.com>
2024-08-11 17:55:36 +0800

    update grunt commit process

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-screeps");
 
     grunt.initConfig({
-        screeps: {
+        default: {
             options: {
                 email: "2973720373@qq.com",
                 token: "6dbf038c-0ef8-4f26-a1aa-8edf92e42665",
