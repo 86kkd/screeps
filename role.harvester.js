31bcfdc0eb2a5ed32a7f58032fa88c76601c60ba
86kkd <2973720373@qq.com>
2024-08-11 16:17:33 +0800

    update icon error while code

--- role.harvester.js
+++ role.harvester.js
@@ -1,6 +1,6 @@
 const TAG = "HARVESTER :";
 const roleHarvester = {
-    run_to_another_room: function (room_name) {
+    run_to_another_room: function(room_name) {
         room = Game.room[room_name];
         const exitDir = creep.room.findExitTo(room);
         const exit = creep.pos.findClosestByRange(exitDir);
@@ -8,7 +8,7 @@ const roleHarvester = {
     },
 
     /** @param {Creep} creep **/
-    run: function (creep, source_targets, store_filter) {
+    run: function(creep, source_targets, store_filter) {
         // act as a harvester
         // source_targets = "297e3b8710cc0c9";
         if (creep.store[RESOURCE_ENERGY] == 0 && creep.memory.working) {
@@ -38,6 +38,7 @@ const roleHarvester = {
                     creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
                 ) {
                     creep.say("🌾");
+
                     creep.moveTo(targets, {
                         visualizePathStyle: { stroke: "#ffffff" },
                     });
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,21 +1,20 @@
 const roleUpgrader = {
     /** @param {Creep} creep **/
-    run: function (creep, source) {
+    run: function(creep, source) {
         if (creep.memory.working && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.working = false;
-            creep.say("🔄 harvest");
         }
         if (!creep.memory.working && creep.store.getFreeCapacity() == 0) {
             creep.memory.working = true;
-            creep.say("⬆️ upgrade");
         }
 
         if (creep.memory.working) {
             if (
                 creep.upgradeController(creep.room.controller) ==
-                    ERR_NOT_IN_RANGE
+                ERR_NOT_IN_RANGE
             ) {
                 creep.say("⬆️");
+
                 creep.moveTo(creep.room.controller, {
                     visualizePathStyle: { stroke: "#ffffff" },
                 });
